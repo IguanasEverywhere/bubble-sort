4cb978422e6a057067fc4f137a44a7a2b88f8491
Scott Schwab <scottschwab86@gmail.com>
2024-02-09 14:55:08 -0600

    Complete solution

--- javascript/bubble_sort.js
+++ javascript/bubble_sort.js
@@ -1,11 +1,37 @@
 function bubbleSort(arr) {
-  // type your code here
+  // have a pointer var pointing at first element
+  // while pointer is not pointing at last value:
+  // compare value at pointer to value at pointer + 1
+  // if value at pointer > value at pointer + 1, swap and return pointer to beginning
+  // else increment pointer
+  // arr
+
+  if (arr.length === 0) {
+    return arr;
+  }
+
+  let pointer = 0;
+  while (pointer !== arr.length - 1) {
+    if (arr[pointer] > arr[pointer + 1]) {
+      // let temp = arr[pointer];
+      // arr[pointer] = arr[pointer + 1];
+      // arr[pointer + 1] = temp;
+      [arr[pointer], arr[pointer + 1]] = [arr[pointer + 1], arr[pointer]]
+      pointer = 0;
+    } else {
+      pointer++;
+    }
+  }
+
+  return arr;
+
 }
 
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: [1, 2, 3, 4]");
   console.log("=>", bubbleSort([3, 2, 1, 4]));
+
 
   console.log("");
 
@@ -21,6 +47,11 @@ if (require.main === module) {
 
   console.log("Expecting: [1, 2, 3]");
   console.log("=>", bubbleSort([2, 3, 1]));
+
+  console.log("");
+
+  console.log("Expecting: [1, 2, 2, 2, 3]");
+  console.log("=>", bubbleSort([2, 3, 2, 2, 1]));
 }
 
 module.exports = bubbleSort;
